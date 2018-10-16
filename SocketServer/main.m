15572f1c3e4f934f1f1ff33b688beea5668686b3
Dylan Xiao <dylanxiao@dylans-macbook.local>
2018-10-16 22:01:51 +0800

    Initial Commit

---
+++ SocketServer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SocketServer
+//
+//  Created by Dylan Xiao on 2018/10/16.
+//  Copyright © 2018年 Dylan Xiao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
